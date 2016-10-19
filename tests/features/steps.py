20c8ec64bb8457e38f0d45c5056c6faf172d75d9
Basavanagowda Kanur <basavanagowda@gmail.com>
2016-10-19 12:00:49 +0530

    Implement test for title on each page

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -1,5 +1,6 @@
 from lettuce import step, world, before
 from nose.tools import assert_equals
+from lxml import html
 
 from microblog.controllers import app
 
@@ -17,3 +18,10 @@ def when_i_visit_group1_path(step, path):
 @step(u'Then I should get a \'([^\']*)\' response')
 def then_i_should_get_a_group1_response(step, expected_status_code):
     assert_equals(world.response.status_code, int(expected_status_code))
+
+
+@step(u'And title set to \'([^\']*)\'')
+def and_title_set_to_group1(step, expected_title):
+    dom = html.fromstring(world.response.get_data(as_text=True))
+    title = dom.cssselect('title')[0].text
+    assert_equals(title, expected_title)
